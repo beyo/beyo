f5f0a8361276760cda5c5571620daa44c7cbcd1c
Yanick Rochon <yanick.rochon@gmail.com>
2014-10-07 23:22:21 -0400

    Test beyo core with coverage near 100%

--- test/lib/beyo.test.js
+++ test/lib/beyo.test.js
@@ -8,6 +8,7 @@ describe('Beyo Application Framework', function () {
   var Beyo;
 
   before(function () {
+    // need to make this relative (?)
     process.chdir('test/fixtures/simple-app');
 
     Beyo = require('../../lib/beyo');
@@ -19,11 +20,15 @@ describe('Beyo Application Framework', function () {
 
   this.timeout(3000);
 
-  it('should load application', function (done) {
+  describe('Test loading application', function () {
 
-    co(function * () {
-      var testBeyo = new Beyo();
+    var testBeyo;
 
+    before(function () {
+      testBeyo = new Beyo()
+    });
+
+    it('should initialize', function * () {
       testBeyo.isInitializing.should.be.false;
       testBeyo.isReady.should.be.false;
 
@@ -31,17 +36,116 @@ describe('Beyo Application Framework', function () {
 
       testBeyo.isInitializing.should.be.false;
       testBeyo.isReady.should.be.true;
-      //testBeyo.app.should.be.???
 
       // if the fixture was called, then `init` was invoked, and we should have an appRequire function!
       testBeyo.appRequire.should.be.a.Function;
+    });
 
+    it('should have loaded app', function () {
+      testBeyo.app.should.equal('app');
+    });
+
+    it('should have processed post init handlers', function () {
       testBeyo.__postInitTestGenerator.should.be.true;
       testBeyo.__postInitTestThunk.should.be.true;
+    });
 
-    })(done);
+    describe('Test plugins', function () {
+
+      it('should return available plugins', function () {
+        testBeyo.plugins.should.be.a.Function;
+        testBeyo.plugins.available.should.be.an.Array;
+
+        testBeyo.plugins.available.forEach(function (key) {
+          testBeyo.plugins(key).should.be.a.Function;
+          testBeyo.plugins(key)().should.be.true;
+        });
+      });
+
+      it('should not allow modifying available plugin array', function () {
+        (function () {
+          testBeyo.plugins.available.push('test');
+        }).should.throw();
+      });
+
+      it('should fail with invalid plugin key', function () {
+        [
+          undefined, true, false, null, void 0, -1, 0, 1, function () {}, {}, [], /./, ''
+        ].forEach(function (invalidKey) {
+          (function () { testBeyo.plugins(invalidKey); }).should.throw();
+        });
+      });
+
+    });
+
+    describe('Test Models', function () {
+
+      it('should return available models', function () {
+        testBeyo.models.should.be.a.Function;
+        testBeyo.models.available.should.be.an.Array;
+
+        testBeyo.models.available.forEach(function (key) {
+          testBeyo.models(key);   // Note : undefined result, just check that we get it...
+        });
+      });
+
+      it('should fail with invalid model key', function () {
+        [
+          undefined, true, false, null, void 0, -1, 0, 1, function () {}, {}, [], /./, '',
+          'a/b/c'
+        ].forEach(function (invalidKey) {
+          (function () { testBeyo.models(invalidKey); }).should.throw();
+        });
+      });
+
+      it('should fetch model even when no module specified', function () {
+        testBeyo.models('test/Foo').should.be.equal(testBeyo.models('Foo'));
+      });
+
+    });
+
+    describe('Test Services', function () {
+
+      it('should return available services', function () {
+        testBeyo.services.should.be.a.Function;
+        testBeyo.services.available.should.be.an.Array;
+
+        testBeyo.services.available.forEach(function (key) {
+          testBeyo.services(key);   // Note : undefined result, just check that we get it...
+        });
+      });
+
+      it('should fail with invalid service key', function () {
+        [
+          undefined, true, false, null, void 0, -1, 0, 1, function () {}, {}, [], /./, '',
+          'a/b/c'
+        ].forEach(function (invalidKey) {
+          (function () { testBeyo.services(invalidKey); }).should.throw();
+        });
+      });
+
+      it('should fetch model even when no module specified', function () {
+        testBeyo.services('test/index').should.be.equal(testBeyo.services('index'));
+      });
+
+    });
+
   });
 
+  describe('Test environment settings', function () {
+
+    it('should allow changing value', function () {
+      var testBeyo = new Beyo();
+
+      testBeyo.env.should.equal('development');
+
+      testBeyo.env = 'test';
+
+      testBeyo.env.should.equal('test');
+      process.env.NODE_ENV.should.equal(testBeyo.env);
+    });
+
+  });
 
 
 });
